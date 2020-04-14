1517b200c3a1ab0bb8061c6f166f3c56bd535a3e
tzoitzeot <tristan.maciag@icloud.com>
2020-04-14 07:30:57 +0200

    create ListMyGameCard component & add MyGameCard component (only static display)

--- src/components/contents/MyGames.jsx
+++ src/components/contents/MyGames.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import ListMyGameCards from './my-games/ListMyGameCards';
 import './mygames.scss';
 
 const MyGames = () => {
-  return <div className="my-games">my game page</div>;
+  return (
+    <div className="my-games">
+      <ListMyGameCards />
+    </div>
+  );
 };
 
 // MyGames.propTypes = {};
---
+++ src/components/contents/my-games/ListMyGameCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types'
+import MyGameCard from './MyGameCard';
+
+const ListMyGameCards = () => {
+  return (
+    <div>
+      <MyGameCard />
+    </div>
+  );
+};
+
+// ListMyGameCards.propTypes = {
+
+// }
+
+export default ListMyGameCards;
