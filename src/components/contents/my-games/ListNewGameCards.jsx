2c01a4f33e3988103cbb0fc2bd218c83138f3325
tzoitzeot <tristan.maciag@icloud.com>
2020-04-15 08:28:21 +0200

    dynamique NewGameCard component display

--- src/components/contents/my-games/ListNewGameCards.jsx
+++ src/components/contents/my-games/ListNewGameCards.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
 // import PropTypes from 'prop-types'
 import NewGameCard from './NewGameCard';
+import games from '../../data/games.json';
+
+const displayNewGameCards = () =>
+  games.map(game => (
+    <div>
+      <NewGameCard {...game} />
+    </div>
+  ));
 
 const ListNewGameCards = () => {
-  return (
-    <div className="grid-cards-display">
-      <div>
-        <NewGameCard />
-      </div>
-      <div>
-        <NewGameCard />
-      </div>
-      <div>
-        <NewGameCard />
-      </div>
-      <div>
-        <NewGameCard />
-      </div>
-      <div>
-        <NewGameCard />
-      </div>
-      <div>
-        <NewGameCard />
-      </div>
-    </div>
-  );
+  return <div className="grid-cards-display">{displayNewGameCards()}</div>;
 };
 
 //  ListNewGameCards.propTypes = {
