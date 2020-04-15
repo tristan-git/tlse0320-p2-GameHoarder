0f74202c65fbcba2e4c951919350fe5cf68f0b0b
Leslie <lesliec38@hotmail.fr>
2020-04-15 11:33:19 +0200

    change MyGameCard

--- src/components/contents/my-games/ListMyGameCards.jsx
+++ src/components/contents/my-games/ListMyGameCards.jsx
@@ -6,22 +6,52 @@ const ListMyGameCards = () => {
   return (
     <div className="grid-cards-display">
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="Who Wants to be a Millionaire: 1st Edition"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/haz3ksilxulos1kcdpux.jpg"
+          popularity="1"
+          rating="43.0"
+        />
       </div>
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="Woodpunk"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/yvepazkit8g5bcmeur7b.jpg"
+          popularity="2"
+          rating="70.0"
+        />
       </div>
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="Oppai Puzzle"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/s4ymcvgk9lhmapvaeprl.jpg"
+          popularity="1.0"
+          rating="70.0"
+        />
       </div>
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="Panzer Corps"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/efukb4yrq64r659oujwa.jpg"
+          popularity="1"
+          rating="80"
+        />
       </div>
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="DreadEye VR"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/ptxwv7se4ponk3eo9wqr.jpg"
+          popularity="1"
+          rating="70"
+        />
       </div>
       <div>
-        <MyGameCard />
+        <MyGameCard
+          name="Beasts of Bermuda"
+          url="//images.igdb.com/igdb/image/upload/t_thumb/omzxoh8snokejg2psykk.jpg"
+          popularity="2"
+          rating="50"
+        />
       </div>
     </div>
   );
--- src/components/contents/my-games/MyGameCard.jsx
+++ src/components/contents/my-games/MyGameCard.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 // import PropTypes from 'prop-types'
 
-const MyGameCard = () => {
-  return <div>une card composant const MyGameCard</div>;
-};
+class MyGameCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = 'name';
+    this.url = 'url';
+    this.popularity = 'popularity';
+    this.rating = 'rating';
+  }
 
-// MyGameCard.propTypes = {
-
-// }
-
+  render() {
+    return (
+      <section className="MyGameCard">
+        <span>
+          <h3>{this.props.name}</h3>
+        </span>
+        <img alt={this.props.name} style={{ backgroundImage: `url(${this.props.url})` }} />
+        <div>
+          <span>
+            <p>Popularité {this.props.popularity}</p>
+            <p>Notation {this.props.rating}</p>
+          </span>
+        </div>
+      </section>
+    );
+  }
+}
 export default MyGameCard;
