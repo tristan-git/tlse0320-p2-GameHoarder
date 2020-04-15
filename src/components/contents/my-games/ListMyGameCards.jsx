1e068beebba76d4cf621e3a18de5925e362169e8
Leslie <lesliec38@hotmail.fr>
2020-04-15 15:08:25 +0200

    change display of the card MyGameCard

--- src/components/contents/my-games/ListMyGameCards.jsx
+++ src/components/contents/my-games/ListMyGameCards.jsx
@@ -9,48 +9,36 @@ const ListMyGameCards = () => {
         <MyGameCard
           name="Who Wants to be a Millionaire: 1st Edition"
           url="//images.igdb.com/igdb/image/upload/t_thumb/haz3ksilxulos1kcdpux.jpg"
-          popularity="1"
-          rating="43.0"
         />
       </div>
       <div>
         <MyGameCard
           name="Woodpunk"
           url="//images.igdb.com/igdb/image/upload/t_thumb/yvepazkit8g5bcmeur7b.jpg"
-          popularity="2"
-          rating="70.0"
         />
       </div>
       <div>
         <MyGameCard
           name="Oppai Puzzle"
           url="//images.igdb.com/igdb/image/upload/t_thumb/s4ymcvgk9lhmapvaeprl.jpg"
-          popularity="1.0"
-          rating="70.0"
         />
       </div>
       <div>
         <MyGameCard
           name="Panzer Corps"
           url="//images.igdb.com/igdb/image/upload/t_thumb/efukb4yrq64r659oujwa.jpg"
-          popularity="1"
-          rating="80"
         />
       </div>
       <div>
         <MyGameCard
           name="DreadEye VR"
           url="//images.igdb.com/igdb/image/upload/t_thumb/ptxwv7se4ponk3eo9wqr.jpg"
-          popularity="1"
-          rating="70"
         />
       </div>
       <div>
         <MyGameCard
           name="Beasts of Bermuda"
           url="//images.igdb.com/igdb/image/upload/t_thumb/omzxoh8snokejg2psykk.jpg"
-          popularity="2"
-          rating="50"
         />
       </div>
     </div>
