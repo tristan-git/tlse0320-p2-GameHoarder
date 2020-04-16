2555dfb844539bcc0675c7f33bd12d32aad25bd9
Leslie <lesliec38@hotmail.fr>
2020-04-16 16:59:46 +0200

    commit for nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import Header from './components/header/Header';
 import MyGames from './components/contents/MyGames';
 import NewGames from './components/contents/NewGames';
 import Footer from './components/footer/Footer';
 import MobileNav from './components/mobile-nav/MobileNav';
-
 import './App.scss';
+
+library.add(fas);
 
 function App() {
   return (
