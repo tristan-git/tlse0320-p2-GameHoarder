4bca6d028112ee8517d377d3966ab2e771cb5cc5
BenjaminDexant <benjamin.dexant@gmail.com>
2020-04-16 21:41:44 +0200

    component new game alright

--- src/components/contents/my-games/NewGameCard.jsx
+++ src/components/contents/my-games/NewGameCard.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-// import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types';
 
-const NewGameCard = () => {
-  return <div>une card composant const NewGameCard</div>;
-};
+class NewGameCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = 'name';
+    this.url = 'url';
+    this.rating = 'rating';
+  }
+
+  render() {
+    return (
+      <div className="Card">
+        <div className="ImageCard">
+          <img className="ImageGame" src={this.props.url} alt="Game cover." />
+        </div>
+        <div className="GameInfo">
+          <h3 className="GameName">{this.props.name}</h3>
+          <p className="GameSupport">Game support</p>
+          <div className="GameRating">Game Rating</div>
+          <div className="ButtonAddLibrary">Add to library</div>
+        </div>
+        <div className="ButtonAddWishlist">Add to wishlist</div>
+      </div>
+    );
+  }
+}
 
 // NewGameCard.propTypes = {
-
-// }
+//   avatarName: PropTypes.string,
+//   avatarImg: PropTypes.string,
+//   avatarOnline: PropTypes.bool
+// };
 
 export default NewGameCard;
