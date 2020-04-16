222887f5e558d7587ffecd3c54eb244495300445
Waledak <tanguy.cirillo@gmail.com>
2020-04-16 09:44:49 +0200

    résolution d'erreur eslint et ajout de proptypes sur le composant title

--- src/components/contents/title/Title.jsx
+++ src/components/contents/title/Title.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import './title.scss';
+import PropTypes from 'prop-types';
 
 export default class Title extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: props.title
+      title: props.title,
+      span: props.span
     };
   }
 
   render() {
+    const { title, span } = this.state;
     return (
       <div>
         <h2 className="title">
-          {this.state.title}
-          <span className="span">{this.props.span}</span>
+          {title}
+          <span className="span">{span}</span>
         </h2>
-        <div className="underlineTitle"></div>
+        <div className="underlineTitle">&nbsp;</div>
       </div>
     );
   }
 }
+
+Title.propTypes = {
+  title: PropTypes.element.isRequired,
+  span: PropTypes.element.isRequired
+};
