3f30dea0efafc0709cb23ddbca80fff4665c7681
BenjaminDexant <benjamin.dexant@gmail.com>
2020-04-17 10:37:46 +0200

    ready to pull and push

--- src/components/contents/my-games/NewGameCard.jsx
+++ src/components/contents/my-games/NewGameCard.jsx
@@ -1,36 +1,49 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+// import StarRating from './StarRating';
+import PropTypes from 'prop-types';
 
 class NewGameCard extends React.Component {
   constructor(props) {
     super(props);
-    this.name = 'name';
-    this.url = 'url';
-    this.rating = 'rating';
+  }
+
+  getDataGame(event) {
+    const getDataNode = event.target.parentNode.parentNode;
+    const img = getDataNode.children[1].style.cssText;
+    const title = getDataNode.children[0].children[0].innerText;
+    const values = {
+      title,
+      img
+    };
+    localStorage.setItem(title, JSON.stringify(values));
   }
 
   render() {
+    const { name } = this.props;
+    const { url } = this.props;
+
     return (
       <div className="Card">
-        <div className="ImageCard">
-          <img className="ImageGame" src={this.props.url} alt="Game cover." />
-        </div>
+        <div className="ImageCard" style={{ backgroundImage: `url(${url})` }} />
         <div className="GameInfo">
-          <h3 className="GameName">{this.props.name}</h3>
+          <div className="GameInfoTitle">
+            <h3 className="GameName">{name}</h3>
+            <div className="ButtonAddWishlist">Wishlist</div>
+          </div>
           <p className="GameSupport">Game support</p>
           <div className="GameRating">Game Rating</div>
-          <div className="ButtonAddLibrary">Add to library</div>
+          <div className="ButtonAddLibrary" onClick={this.getDataGame}>
+            Add to library
+          </div>
         </div>
-        <div className="ButtonAddWishlist">Add to wishlist</div>
       </div>
     );
   }
 }
 
-// NewGameCard.propTypes = {
-//   avatarName: PropTypes.string,
-//   avatarImg: PropTypes.string,
-//   avatarOnline: PropTypes.bool
-// };
+NewGameCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+};
 
 export default NewGameCard;
