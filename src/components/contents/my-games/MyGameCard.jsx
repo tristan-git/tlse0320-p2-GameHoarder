21df6f5cad62e831c0f747789045b5876d5df81f
wilder <er.feltesse@gmail.com>
2020-04-22 13:11:26 +0200

    ajout de l'alerte  confirmation de suppres    const getDataNode = event.target.parentNode.parentNode;sion

--- src/components/contents/my-games/MyGameCard.jsx
+++ src/components/contents/my-games/MyGameCard.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import './MyGameCard.scss';
 import PropTypes from 'prop-types';
+import Swal from 'sweetalert2';
 import StarRating from './StarRating';
 
 class MyGameCard extends React.Component {
@@ -17,7 +18,20 @@ class MyGameCard extends React.Component {
       title,
       imgUrl
     };
-    localStorage.removeItem(title, JSON.stringify(values));
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(result => {
+      if (result.value) {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+        localStorage.removeItem(title, JSON.stringify(values));
+      }
+    });
   }
 
   render() {
