e2cb7c4f96ea30a2209ed157619e9c3e9f7475f6
Waledak <tanguy.cirillo@gmail.com>
2020-04-23 16:58:31 +0200

    utilisation des propos dans le composant NewGameCard et légère modif dans MyGameCard, cette modif permette une meilleure lisibilité du code et d'afficher la note dans l'header de la bibliothèque

--- src/components/contents/my-games/NewGameCard.jsx
+++ src/components/contents/my-games/NewGameCard.jsx
@@ -4,19 +4,18 @@ import PropTypes from 'prop-types';
 class NewGameCard extends React.Component {
   constructor(props) {
     super(props);
+    this.getDataGame = this.getDataGame.bind(this);
   }
 
-  getDataGame(event) {
-    const getDataNode = event.target.parentNode.parentNode;
-    const img = getDataNode.children[0].style.cssText;
-    const title = getDataNode.children[1].children[0].children[0].innerText;
-
-    console.log(img);
-    console.log(title);
+  getDataGame() {
+    const img = this.props.url;
+    const title = this.props.name;
+    const rating = this.props.rating;
 
     const values = {
       title,
-      img
+      img,
+      rating
     };
 
     console.log(values);
@@ -26,7 +25,7 @@ class NewGameCard extends React.Component {
   render() {
     const { name } = this.props;
     const { url } = this.props;
-
+    const { rating } = this.props;
     return (
       <div className="Card">
         <div className="ImageCard" style={{ backgroundImage: `url(${url})` }} />
@@ -36,7 +35,7 @@ class NewGameCard extends React.Component {
             <div className="ButtonAddWishlist">Wishlist</div>
           </div>
           <p className="GameSupport">Game support</p>
-          <div className="GameRating">Game Rating</div>
+          <div className="GameRating">{rating / 10 / 2}</div>
           <div className="ButtonAddLibrary" onClick={this.getDataGame}>
             Ajouter à ma bibliothèque
           </div>
