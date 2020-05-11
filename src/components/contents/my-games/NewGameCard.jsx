82a130ce5566a9b89cbabe1c17db17f1a1542776
Feltesse <er.feltesse@gmail.com>
2020-05-11 14:32:56 +0200

    adding modal

--- src/components/contents/my-games/NewGameCard.jsx
+++ src/components/contents/my-games/NewGameCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Modal from './Modal';
 import PropTypes from 'prop-types';
 
 class NewGameCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      show: false,
+      isWishlist: false
+    };
     this.getDataGame = this.getDataGame.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   getDataGame() {
@@ -20,17 +27,40 @@ class NewGameCard extends React.Component {
     handleGamesList(values);
   }
 
+  handleClick() {
+    this.setState(state => ({
+      isWishlist: !state.isWishlist,
+      show: !state.show
+    }));
+    this.getDataGame();
+  }
+
+  hideModal() {
+    this.setState({ show: false });
+  }
+
   render() {
     const { rating, name, url, addToLib } = this.props;
+    const { isWishlist, show } = this.state;
     return (
       <div className="Card">
         <div className="ImageCard" style={{ backgroundImage: `url(${url})` }} />
         <div className="GameInfo">
           <div className="GameInfoTitle">
             <h3 className="GameName">{name}</h3>
-            <div className="ButtonAddWishlist">
-              <img src="/img/svg/wishlist.svg" alt="icon whislist" />
-            </div>
+            <Modal show={show} handleClose={this.hideModal} />
+            <button
+              value={isWishlist}
+              type="button"
+              onClick={this.handleClick}
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer'
+              }}
+            >
+              <img src="/img/svg/wishlist.svg" alt="icon wishlist" />
+            </button>
           </div>
           <p className="GameSupport">Game support</p>
           <div className="GameRating">{rating / 10 / 2}</div>
