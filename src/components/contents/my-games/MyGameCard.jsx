9821dacf4a14631dbfc044ff2d1122ffd018e275
tzoitzeot <tristan.maciag@icloud.com>
2020-05-13 11:14:04 +0200

    add getSelectValue methode

--- src/components/contents/my-games/MyGameCard.jsx
+++ src/components/contents/my-games/MyGameCard.jsx
@@ -3,41 +3,69 @@ import './MyGameCard.scss';
 import DisplayRating from './DisplayRating';
 import PropTypes from 'prop-types';
 
-function MyGameCard(props) {
-  const { img, title, rating } = props.data;
-  return (
-    <section className="Card">
-      <div className="header">
-        <h3>{title}</h3>
-        <button onClick={() => props.gameToRemove(title)} className="close-btn" type="submit">
-          <img
-            src="./img/svg/delete-black.svg"
-            alt="delete"
-            style={{ backgroundColor: 'transparent' }}
-          />
-        </button>
-      </div>
-      <div className="image" style={{ backgroundImage: `url(${img[0]})` }} />
-      <div className="footer">
-        <div className="ratingSuggestion">
-          <DisplayRating rating={rating} />
-        </div>
+class MyGameCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValueStatus: ''
+    };
 
-        <select name="statuts" className="statuts">
-          <option value="Statuts">STATUTS</option>
-          <option value="Pas commencé">Pas commencé</option>
-          <option value="En cours">En cours</option>
-          <option value="Terminé">Terminé</option>
-        </select>
-      </div>
-    </section>
-  );
+    this.getSelectValue = this.getSelectValue.bind(this);
+  }
+
+  getSelectValue(event) {
+    const valueToChange = event.target.value;
+    this.setState({ selectValueStatus: valueToChange });
+    const { img, title, rating, id, platformsName } = this.props.data;
+    const values = {
+      addingDate: new Date(),
+      title,
+      img,
+      rating,
+      id,
+      addToLib: true,
+      addToWish: false,
+      platformsName,
+      status: valueToChange
+    };
+  }
+
+  render() {
+    const { props } = this;
+    const { img, title, rating } = props.data;
+    return (
+      <section className="Card">
+        <div className="header">
+          <h3>{title}</h3>
+          <button onClick={() => props.gameToRemove(title)} className="close-btn" type="submit">
+            <img
+              src="./img/svg/delete-black.svg"
+              alt="delete"
+              style={{ backgroundColor: 'transparent' }}
+            />
+          </button>
+        </div>
+        <div className="image" style={{ backgroundImage: `url(${img[0]})` }} />
+        <div className="footer">
+          <div className="ratingSuggestion">
+            <DisplayRating rating={rating} />
+          </div>
+
+          <select name="statuts" className="statuts" onChange={this.getSelectValue}>
+            <option value="Statuts">STATUTS</option>
+            <option value="Pas commencé">Pas commencé</option>
+            <option value="En cours">En cours</option>
+            <option value="Terminé">Terminé</option>
+          </select>
+        </div>
+      </section>
+    );
+  }
 }
 
 MyGameCard.propTypes = {
-  data: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
-  url: PropTypes.array.isRequired,
+  url: PropTypes.string.isRequired,
   gameToRemove: PropTypes.func.isRequired
 };
 export default MyGameCard;
