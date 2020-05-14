929c25157868249cbb37e44706151d7abce9dd3c
tzoitzeot <tristan.maciag@icloud.com>
2020-05-14 07:50:06 +0200

    fix conflicts between addWishlist/removeWishlist btn & addLibrary/removeLibrary btn

--- src/App.jsx
+++ src/App.jsx
@@ -74,18 +74,27 @@ class App extends React.Component {
   }
 
   handleGamesList(values) {
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        listGamesLib: [...prevState.listGamesLib, values]
-      };
+    // this.setState(prevState => {
+    //   return {
+    //     ...prevState,
+    //     listGamesLib: [...prevState.listGamesLib, values]
+    //   };
+    // });
+
+    const { listGamesLib } = this.state;
+    let newlistGamesLib = listGamesLib;
+    newlistGamesLib = newlistGamesLib.filter(game => game.title !== values.title);
+    this.setState({
+      listGamesLib: [...newlistGamesLib, values]
     });
   }
 
   handleWishlistGame(values) {
     const { listGamesLib } = this.state;
+    let newlistGamesLib = listGamesLib;
+    newlistGamesLib = newlistGamesLib.filter(game => game.title !== values.title);
     this.setState({
-      listGamesLib: [...listGamesLib, values]
+      listGamesLib: [...newlistGamesLib, values]
     });
   }
 
